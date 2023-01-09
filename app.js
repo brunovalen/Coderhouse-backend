393642731d3972b1425e175d6d89c80bc0646fab
bruno <bruno@MacBook-Air.local>
2023-01-08 22:08:18 -0300

    Login

--- app.js
+++ app.js
@@ -1,21 +1,84 @@
-const express = require('express');
-const _ = require('lodash');
-const logger = require('morgan');
-require('dotenv').config();
-
-const errorHandler = require('./src/middlewares/errorHandler');
-const productRoutes = require('./src/routes/products/products.routes')
-const chatsRoutes = require('./src/routes/chats/chats.routes')
+const express = require("express");
+const session = require("express-session");
+const expbs = require("express-handlebars");
+require("dotenv").config({ path: "./config/.env" });
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
+const path = require("path");
+const routes = require("./routers/index");
 
 const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
 
+/*      PERSISTENCIA POR MONGO ATLAS     */
+const MongoStore = require("connect-mongo");
+const adavancedOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+/* ------------------------------------- */
+
+//Session config
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGOATLAS,
+      mongoOptions: adavancedOptions,
+    }),
+    secret: "secreto",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(logger('dev'));
+app.use(express.static("./views/layouts"));
+app.use("/", routes);
 
-app.use('/api/products', productRoutes);
-app.use('/api/chats', chatsRoutes);
+//Motor de plantillas
+app.engine(
+  "hbs",
+  expbs.engine({
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "views/partials"),
+    extname: ".hbs",
+  })
+);
+app.set("views", "./views");
+app.set("views engine", "hbs");
 
-app.use(errorHandler);
+/* CHAT */
+const ApiChat = require("./api/apiChat");
+const apiChat = new ApiChat();
+let messages = [];
 
-module.exports = app;
+io.on("connection", async (socket) => {
+  let messagesToEmit = await apiChat.readChatFromFile();
+
+  messages.splice(0, messages.length);
+  for (const m of messagesToEmit) {
+    messages.push(m);
+  }
+
+  socket.emit("messages", messagesToEmit);
+
+  socket.on("new-message", (data) => {
+    data.id = messages.length+1
+    messages.push(data);
+
+    io.sockets.emit("messages", [data]);
+
+    apiChat.writeChatToFile(messages);
+  });
+});
+
+//Manejador de errores
+app.use(function (err, req, res, next) {
+  console.log(err.stack);
+  res.status(500).send("Ocurrio un error: " + err);
+});
+
+//Server
+httpServer.listen(process.env.PORT || 8080, () => {
+  console.log("SERVER ON");
+});
